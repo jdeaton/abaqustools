cea8668522bc36cf1958458e014cbee7b2cdd491
Josh Deaton <jdeaton@users.noreply.github.com>
2018-02-18 23:42:17 -0500

    Added global variable to specify location of abaqus_script_files.

--- abaqustools/__init__.py
+++ abaqustools/__init__.py
@@ -1,1 +1,6 @@
 # Copyright (c) 2018 Joshua Deaton. All Rights Reserved.
+
+import os.path as path
+
+ABAQUS_SCRIPTS_DIR = path.abspath(path.join(path.dirname(__file__),
+                                            "..", "abaqus_python_scripts"))
