9cbf98596b1751d17a7e92c24a0319706e8c849f
Josh Deaton <jdeaton@users.noreply.github.com>
2018-02-19 13:15:49 -0500

    Quick implementation of function to get natural frequencies.

--- abaqustools/__init__.py
+++ abaqustools/__init__.py
@@ -1,6 +1,21 @@
 # Copyright (c) 2018 Joshua Deaton. All Rights Reserved.
 
+import subprocess
+import csv
 import os.path as path
+
 
 ABAQUS_SCRIPTS_DIR = path.abspath(path.join(path.dirname(__file__),
                                             "..", "abaqus_python_scripts"))
+
+
+def get_frequencies(odb_name, stepname):
+    system_string = "abaqus python " + \
+                    path.join(ABAQUS_SCRIPTS_DIR, "abaqus_get_step_frequency_csv.py") + \
+                    " " + odb_name + " " + stepname
+    subprocess.Popen(system_string, shell=True)
+
+    with open("frequency.csv") as csv_file:
+        frequencies = [row[0] for row in csv.reader(csv_file)]
+
+    return frequencies
